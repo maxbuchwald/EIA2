3b2f2f03b83fe9853d9ab2e96f5da9d68c125cf0
maxbuchwald <you@example.com>
2020-05-09 21:48:50 +0200

    .

--- Aufgabe_03/Nachbarschaftshilfe/Nachbarschaftshilfe.js
+++ Aufgabe_03/Nachbarschaftshilfe/Nachbarschaftshilfe.js
@@ -7,40 +7,49 @@ var L03_CocktailBar;
         let form = document.querySelector("div#form");
         form.addEventListener("change", handleChange);
     }
-    function handleChange(_event) {
+    function handleChange() {
         let order = document.querySelector("div#order");
         order.innerHTML = "";
         let formData = new FormData(document.forms[0]);
+        let householdprice = 0;
+        let withdrawalprice = 0;
+        let shoppingprice = 0;
         for (let entry of formData) {
             if (entry[0] == "supermarket") {
                 let supermarket = document.querySelector("[value='" + entry[1] + "']");
                 order.innerHTML += supermarket.value + "<br>";
-                // console.log(entry[0], entry[1]);
-                // continue;
             }
             if (entry[0] == "household") {
                 if (entry[1] != null && entry[1] != "") {
                     let household = document.querySelector("[value='" + entry[1] + "']");
                     let price = Number(household.getAttribute("price"));
-                    order.innerHTML += household.value + " " + price + " €" + "<br>";
-                    // continue;
+                    householdprice += price;
+                    order.innerHTML += household.value + " " + price + "€" + "<br>";
                 }
             }
             if (entry[0] == "money") {
                 if (entry[1] != null && entry[1] != "") {
-                    let money = document.querySelector("[value='" + entry[1] + "']");
+                    let money = document.querySelector("[name='" + entry[0] + "']");
                     let pricemoney = Number(money.value);
-                    console.log(pricemoney);
+                    let fee = 5;
+                    if (pricemoney == 0)
+                        fee = 0;
+                    withdrawalprice += pricemoney + fee,
+                        order.innerHTML += "Withdrawal " + pricemoney + "€ + fee " + fee + " € " + "<br>";
                 }
             }
             if (entry[0] == "shopping") {
                 if (entry[1] != null && entry[1] != "") {
                     let item = document.querySelector("[value='" + entry[1] + "']");
                     let price = Number(item.getAttribute("price"));
-                    order.innerHTML += item.value + " " + price + " € " + "<br>";
+                    shoppingprice += price;
+                    order.innerHTML += item.value + " " + price + "€ " + "<br>";
                 }
             }
         }
+        let bill = 0;
+        bill += householdprice + withdrawalprice + shoppingprice;
+        order.innerHTML += "<br>" + "total " + bill + "€";
     }
 })(L03_CocktailBar || (L03_CocktailBar = {}));
 //# sourceMappingURL=Nachbarschaftshilfe.js.map
--- Aufgabe_03/Nachbarschaftshilfe/Nachbarschaftshilfe.ts
+++ Aufgabe_03/Nachbarschaftshilfe/Nachbarschaftshilfe.ts
@@ -1,6 +1,7 @@
 namespace L03_CocktailBar {
     window.addEventListener("load", handleLoad);
 
+
     function handleLoad(_event: Event): void {
         // console.log("Start");
         let form: HTMLDivElement = <HTMLDivElement>document.querySelector("div#form");
@@ -8,46 +9,49 @@ namespace L03_CocktailBar {
 
     }
 
-    function handleChange(_event: Event): void {
+    function handleChange(): void {
         let order: HTMLDivElement = <HTMLDivElement>document.querySelector("div#order");
         order.innerHTML = "";
 
         let formData: FormData = new FormData(document.forms[0]);
-
+        let householdprice: number = 0;
+        let withdrawalprice: number = 0;
+        let shoppingprice: number = 0;
         for (let entry of formData) {
-
             if (entry[0] == "supermarket") {
                 let supermarket: HTMLInputElement = <HTMLInputElement>document.querySelector("[value='" + entry[1] + "']");
                 order.innerHTML += supermarket.value + "<br>";
-                // console.log(entry[0], entry[1]);
-                // continue;
             }
             if (entry[0] == "household") {
                 if (entry[1] != null && entry[1] != "") {
                     let household: HTMLInputElement = <HTMLInputElement>document.querySelector("[value='" + entry[1] + "']");
                     let price: number = Number(household.getAttribute("price"));
-                    order.innerHTML += household.value + " " + price + " €" + "<br>";
-                    // continue;
+                    householdprice += price;
+                    order.innerHTML += household.value + " " + price + "€" + "<br>";
                 }
             }
-
             if (entry[0] == "money") {
                 if (entry[1] != null && entry[1] != "") {
-                    let money: HTMLInputElement = <HTMLInputElement>document.querySelector("[value='" + entry[1] + "']");
+                    let money: HTMLInputElement = <HTMLInputElement>document.querySelector("[name='" + entry[0] + "']");
                     let pricemoney: number = Number(money.value);
-                    console.log(pricemoney);
+                    let fee: number = 5;
+                    if (pricemoney == 0)
+                        fee = 0;
+                    withdrawalprice += pricemoney + fee,
+                        order.innerHTML += "Withdrawal " + pricemoney + "€ + fee " + fee + " € " + "<br>";
                 }
             }
             if (entry[0] == "shopping") {
                 if (entry[1] != null && entry[1] != "") {
                     let item: HTMLInputElement = <HTMLInputElement>document.querySelector("[value='" + entry[1] + "']");
                     let price: number = Number(item.getAttribute("price"));
-                    order.innerHTML += item.value + " " + price + " € " + "<br>";
+                    shoppingprice += price;
+                    order.innerHTML += item.value + " " + price + "€ " + "<br>";                     
                 }
             }
-
-
-
         }
+        let bill: number = 0;
+        bill += householdprice + withdrawalprice + shoppingprice;
+        order.innerHTML += "<br>" + "total " + bill + "€";
     }
 }
